30ccfae680561917ea7fd780968e7525e28cb7b1
unknown <lari.felt@edu.keuda.fi>
2023-09-18 11:24:42 +0300

    koodari1

--- koodaushaasteet/koodaushaaste1.js
+++ koodaushaasteet/koodaushaaste1.js
@@ -12,7 +12,7 @@ var taulukko = [8, 4, 9, 6, 2, 7, 1, 5];
 var tulos = pieninSuurin(taulukko);
 console.log(tulos);
 
-
+console.log()
 
 function ParTon(numero) {
     if (numero % 2 === 0) {
@@ -21,91 +21,148 @@ function ParTon(numero) {
         return "pariton";
     }
 }
+var tulos = ParTon(52);
+console.log(tulos);
 
-var vastaus = ParTon(12)
-console.log(vastaus);
+console.log()
 
-
-
-function kuukausi(kuukaudenNumero) {
-    var kuukaudet = [
-        "tammikuu",
-        "helmikuu",
-        "maaliskuu",
-        "huhtikuu",
-        "toukokuu",
-        "kesäkuu",
-        "heinäkuu",
-        "elokuu",
-        "syyskuu",
-        "lokakuu",
-        "marraskuu",
-        "joulukuu"
-    ];
-
-    if (kuukaudenNumero >= 1 && kuukaudenNumero <= 12) {
-        return kuukaudet[kuukaudenNumero - 1];
+function kuukausi(kk)
+{
+    switch (kk)
+    {
+        case 1: console.log("Tammikuu");
+            break;
+        case 2: console.log("Helmikuu");
+            break;
+        case 3: console.log("Maaliskuu");
+            break;
+        case 4: console.log("Huhtikuu");
+            break;
+        case 5: console.log("Toukokuu");
+            break;
+        case 6: console.log("Kesäkuu");
+            break;
+        case 7: console.log("Heinäkuu");
+            break;
+        case 8: console.log("Elokuu");
+            break;
+        case 9: console.log("Syyskuu");
+            break;
+        case 10: console.log("Lokakuu");
+            break;
+        case 11: console.log("Marraskuu");
+            break;
+        case 12: console.log("Joulukuu");
+            break;
+        default:
+            console.log("Anna kuukauden numero 1-12")
     }
 }
-var kuukausiNimi = kuukausi(9)
-console.log(kuukausiNimi);
+kuukausi(3);
+
+console.log()
 
 
 
-function yhteystiedot(henkilot) {
-    var osoitelista = [];
 
-    for (var henkilo of henkilot) {
-        var { etunimi, sukunimi, lahiosoite, postinumero, postitoimipaikka, puhelin, sahkoposti } = henkilo;
+    function yhteystiedot(henkilot) {
+        var osoitelista = [];
 
-        var osoitetieto = {
-            Etunimi: etunimi,
-            Sukunimi: sukunimi,
-            Lahiosoite: lahiosoite,
-            Postinumero: postinumero,
-            Postitoimipaikka: postitoimipaikka,
-            Puhelin: puhelin,
-            Sahkoposti: sahkoposti
-        };
+        for (var henkilo of henkilot) {
+            var { etunimi, sukunimi, lahiosoite, postinumero, postitoimipaikka, puhelin, sahkoposti } = henkilo;
 
-        osoitelista.push(osoitetieto);
+            var osoitetieto = {
+                Etunimi: etunimi,
+                Sukunimi: sukunimi,
+                Lahiosoite: lahiosoite,
+                Postinumero: postinumero,
+                Postitoimipaikka: postitoimipaikka,
+                Puhelin: puhelin,
+                Sahkoposti: sahkoposti
+            };
+
+            osoitelista.push(osoitetieto);
+        }
+
+        return osoitelista;
     }
 
-    return osoitelista;
+    var henkilo1 = {
+        etunimi: "Matti",
+        sukunimi: "Menninkäinen",
+        lahiosoite: "Kivenkolo 1",
+        postinumero: "99500",
+        postitoimipaikka: "Metsä",
+        puhelin: "0401234567",
+        sahkoposti: "matti.menninkäinen@esimerkki.fi"
+    };
+
+    var henkilo2 = {
+        etunimi: "Anna",
+        sukunimi: "Mäki",
+        lahiosoite: "Risuinentie 56",
+        postinumero: "04300",
+        postitoimipaikka: "Tuusula",
+        puhelin: "0409876543",
+        sahkoposti: "mäen.anna@esimerkki.fi"
+    };
+
+    var henkilo3 = {
+        etunimi: "Tero",
+        sukunimi: "Testaaja",
+        lahiosoite: "Keskikatu 3",
+        postinumero: "04200",
+        postitoimipaikka: "Kerava",
+        puhelin: "0401122334",
+        sahkoposti: "testo.tero@esimerkki.fi"
+    };
+
+    var henkilot = [henkilo1, henkilo2, henkilo3];
+
+    var osoitteet = yhteystiedot(henkilot);
+
+    console.log(osoitteet);
+
+
+    function aakkoset(Sana) {
+
+        var kirjaimet = Array.from(Sana);
+
+        kirjaimet.sort();
+
+        var jarjestettySana = kirjaimet.join('');
+
+        console.log(jarjestettySana);
+    }
+aakkoset("webmaster");
+
+console.log()
+
+function arvosana(nimi, pisteet) {
+    let arvosana;
+
+    if (pisteet < 50) {
+        arvosana = "Hylätty";
+    } else if (pisteet < 60) {
+        arvosana = "T1";
+    } else if (pisteet < 70) {
+        arvosana = "T2";
+    } else if (pisteet < 80) {
+        arvosana = "H3";
+    } else if (pisteet < 90) {
+        arvosana = "H4";
+    } else if (pisteet <= 100) {
+        arvosana = "K5";
+    } else {
+        arvosana = "Virheelliset pisteet";
+    }
+
+    console.log(`${nimi} ${arvosana}`);
 }
 
-var henkilo1 = {
-    etunimi: "Matti",
-    sukunimi: "Menninkäinen",
-    lahiosoite: "Kivenkolo 1",
-    postinumero: "99500",
-    postitoimipaikka: "Metsä",
-    puhelin: "0401234567",
-    sahkoposti: "matti.menninkäinen@esimerkki.fi"
-};
+arvosana("Daniel", 80);
+arvosana("Viivi", 77);
+arvosana("Tiina", 88);
+arvosana("Ismael", 95);
+arvosana("Tuomas", 68);
 
-var henkilo2 = {
-    etunimi: "Anna",
-    sukunimi: "Mäki",
-    lahiosoite: "Risuinentie 56",
-    postinumero: "04300",
-    postitoimipaikka: "Tuusula",
-    puhelin: "0409876543",
-    sahkoposti: "mäen.anna@esimerkki.fi"
-};
-
-var henkilo3 = {
-    etunimi: "Tero",
-    sukunimi: "Testaaja",
-    lahiosoite: "Keskikatu 3",
-    postinumero: "04200",
-    postitoimipaikka: "Kerava",
-    puhelin: "0401122334",
-    sahkoposti: "testo.tero@esimerkki.fi"
-};
-
-var henkilot = [henkilo1, henkilo2, henkilo3];
-
-var osoitteet = yhteystiedot(henkilot);
-
-console.log(osoitteet);
