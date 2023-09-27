b28160b87ef027751094c75c3b0d3fbe2c7c4d0e
unknown <lari.felt@edu.keuda.fi>
2023-09-27 14:21:56 +0300

    keskiviikko

--- PinjaTehtävät/js/viikko3.1.js
+++ PinjaTehtävät/js/viikko3.1.js
@@ -1,86 +1,68 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
-    // Funktio tarkistaa parilliset luvut
-    function tarkistaParilliset() {
-        var syoteElementti = document.getElementById("luku");
-        var syote = parseInt(syoteElementti.value);
+﻿
+// Funktio tarkistaa parilliset luvut
+function tarkistaParilliset() {
+    var syoteElementti = document.getElementById("luku");
+    var syote = parseInt(syoteElementti.value);
 
-        var tuloksetElementti = document.getElementById("parillisetTulokset");
-        tuloksetElementti.textContent = "";
+    var tuloksetElementti = document.getElementById("parillisetTulokset").querySelector("p");
+    tuloksetElementti.textContent = "";
 
-        if (!isNaN(syote) && syote % 2 === 0 && syote > 0) {
-            var parilliset = [];
-            for (var i = 2; i <= syote; i += 2) {
-                if (i !== 0) {
-                    parilliset.push(i);
-                }
+    if (!isNaN(syote) && syote % 2 === 0 && syote > 0) {
+        var parilliset = [];
+        for (var i = 2; i <= syote; i += 2) {
+            if (i !== 0) {
+                parilliset.push(i);
             }
-            if (parilliset.length > 0) {
-                tuloksetElementti.textContent = parilliset.join(", ");
-            } else {
-                tuloksetElementti.textContent = "Ei parillisia lukuja annetulle alueelle.";
-            }
-        } else {
-            alert("Anna parillinen luku suurempi kuin 0.");
         }
-    }
-
-    var tarkistaButton = document.getElementById("tarkistaButton");
-    tarkistaButton.addEventListener("click", tarkistaParilliset);
-
-
-
-    // Funktio muuntaa sanan salasanaksi
-    function muunnaSalasanaksi() {
-        var syoteElementti = document.getElementById("password");
-        var syote = syoteElementti.value;
-
-        var uusiPasswordElementti = document.getElementById("uusiPassword");
-
-        if (syote) {
-            var salasana = "";
-            for (var i = 0; i < syote.length; i++) {
-                if (i % 2 === 1) {
-                    salasana += "Ö" + syote[i].toUpperCase();
-                } else {
-                    salasana += syote[i];
-                }
-            }
-            uusiPasswordElementti.textContent = "Salasana: " + salasana;
+        if (parilliset.length > 0) {
+            tuloksetElementti.textContent = "Parilliset luvut: " + parilliset.join(", ");
         } else {
-            uusiPasswordElementti.textContent = "Syötä sana ensin.";
+            tuloksetElementti.textContent = "Ei parillisia lukuja annetulle alueelle.";
         }
+    } else {
+        alert("Anna parillinen luku suurempi kuin 0.");
     }
+}
+// Funktio muuttaa sanan salasanaksi
+function muunnaSalasanaksi() {
+    var syoteElementti = document.getElementById("password");
+    var syote = syoteElementti.value;
 
-    var passwordButton = document.getElementById("passwordButton");
-    passwordButton.addEventListener("click", muunnaSalasanaksi);
+    var uusiPasswordElementti = document.getElementById("uusiPassword");
+
+    if (syote) {
+        var muunnettuSalasana = "";
+        for (var i = 0; i < syote.length; i++) {
+            muunnettuSalasana += syote[i];
+            muunnettuSalasana += "Ö";
+        }
+        uusiPasswordElementti.textContent = "Salasana: " + muunnettuSalasana;
+    } else {
+        uusiPasswordElementti.textContent = "Syötä sana ensin.";
+    }
+}
 
 
+// Funktio tarkastaa, onko sanassa "ö"-kirjainta
+function tarkastaO() {
+    var syoteElementti = document.getElementById("sana");
+    var syote = syoteElementti.value.toLowerCase();
 
-
-    // Funktio tarkastaa, onko sanassa "ö"-kirjainta
-    function tarkastaO() {
-        var syoteElementti = document.getElementById("sana");
-        var syote = syoteElementti.value.toLowerCase();
-
-        var sanaTuloksetElementti = document.getElementById("sanaTulokset");
+    var sanaTuloksetElementti = document.getElementById("sanaTulokset");
 
         if (syote.includes("ö")) {
-                sanaTuloksetElementti.textContent = "Sanassa on ö-kirjain.";
+            sanaTuloksetElementti.textContent = "Sanassa on ö-kirjain.";
         } else {
-                sanaTuloksetElementti.textContent = "Sanassa ei ole ö-kirjainta.";
+            sanaTuloksetElementti.textContent = "Sanassa ei ole ö-kirjainta.";
         }
-    }
+}
 
-     var sanaButton = document.getElementById("sanaButton");
-     sanaButton.addEventListener("click", tarkastaO);
+// Funktio laskee luvun kertoman
+function laskeKertoma() {
+    var syoteElementti = document.getElementById("kertoma");
+    var syote = parseInt(syoteElementti.value);
 
-
-    // Funktio laskee luvun kertoman
-    function laskeKertoma() {
-        var syoteElementti = document.getElementById("kertoma");
-        var syote = parseInt(syoteElementti.value);
-
-        var kertomaTuloksetElementti = document.getElementById("kertomaTulokset");
+    var kertomaTuloksetElementti = document.getElementById("kertomaTulokset");
 
         if (!isNaN(syote) && syote >= 0) {
             var kertoma = 1;
@@ -91,57 +73,72 @@
         } else {
             kertomaTuloksetElementti.textContent = "Anna positiivinen luku.";
         }
-    }
+}
 
-    var kertomaButton = document.getElementById("kertomaButton");
-    kertomaButton.addEventListener("click", laskeKertoma);
+// Funktio tulostaa luvut ja noudattaa ehtoja
+function tulostaNumerot() {
+    var hipTulosteElementti = document.getElementById("hipTuloste");
+    hipTulosteElementti.innerHTML = "";
 
-
-
-    // Funktio tulostaa luvut ja noudattaa ehtoja
-    function tulostaNumerot() {
-        var hipTulosteElementti = document.getElementById("hipTuloste");
-        hipTulosteElementti.innerHTML = ""; 
-
-        for (var i = 1; i <= 100; i++) {
-            var tuloste = "";
-            if (i % 3 === 0) {
-                tuloste += "hip";
-            }
-            if (i % 5 === 0) {
-                tuloste += "heijaa";
-            }
-            if (tuloste === "") {
-                tuloste = i;
-            }
-            var tulosteElementti = document.createElement("p");
-            tulosteElementti.textContent = tuloste;
-            hipTulosteElementti.appendChild(tulosteElementti);
+    for (var i = 1; i <= 100; i++) {
+        var tuloste = "";
+        if (i % 3 === 0) {
+            tuloste += "hip";
         }
-    }
-
-    var heijaaButton = document.getElementById("heijaaButton");
-    heijaaButton.addEventListener("click", tulostaNumerot);
-
-
-    // Funktio tulostaa 10 ensimmäistä numeroa
-    function tulostaekatNumerot() {
-        var ekatTulosteElementti = document.getElementById("ekatTuloste");
-        ekatTulosteElementti.innerHTML = "";
-
-        for (var i = 1; i <= 10; i++) {
-            var numeroElementti = document.createElement("p");
-            numeroElementti.textContent = i;
-            ekatTulosteElementti.appendChild(numeroElementti);
+        if (i % 5 === 0) {
+            tuloste += "heijaa";
         }
+        if (tuloste === "") {
+            tuloste = i;
+        }
+        var tulosteElementti = document.createElement("p");
+        tulosteElementti.textContent = tuloste;
+        hipTulosteElementti.appendChild(tulosteElementti);
+    }
+}
+
+// Funktio tulostaa 10 ensimmäistä numeroa
+function tulostaekatNumerot() {
+    var ekatTulosteElementti = document.getElementById("ekatTuloste");
+    ekatTulosteElementti.innerHTML = "";
+
+    for (var i = 1; i <= 10; i++) {
+        var numeroElementti = document.createElement("p");
+        numeroElementti.textContent = i;
+        ekatTulosteElementti.appendChild(numeroElementti);
+    }
+}
+
+// Funktio laskee 10 ensimmäistä numeroa yhteen
+function laskeYhteen() {
+    var ekatYhteenTulosteElementti = document.getElementById("ekatYhteenTuloste");
+    ekatYhteenTulosteElementti.innerHTML = "";
+
+    var summa = 0;
+    for (var i = 1; i <= 10; i++) {
+        summa += i;
     }
 
-    var ekatButton = document.getElementById("ekatButton");
-    ekatButton.addEventListener("click", tulostaekatNumerot);
+    var tulosElementti = document.createElement("p");
+    tulosElementti.textContent = "Summa: " + summa;
+    ekatYhteenTulosteElementti.appendChild(tulosElementti);
+}
 
+function laskePotenssi() {
+    var korotettavaElementti = document.getElementById("korotettava");
+    var potenssiElementti = document.getElementById("potenssi");
+    var tuloksetElementti = document.getElementById("potenssiTulokset");
 
-});
+    var korotettava = parseFloat(korotettavaElementti.value);
+    var potenssi = parseFloat(potenssiElementti.value);
 
+    if (!isNaN(korotettava) && !isNaN(potenssi)) {
+        var tulos = Math.pow(korotettava, potenssi);
+        tuloksetElementti.textContent = korotettava + " ^ " + potenssi + " = " + tulos;
+    } else {
+        tuloksetElementti.textContent = "Anna kelvolliset luvut.";
+    }
+}
 
 
 
