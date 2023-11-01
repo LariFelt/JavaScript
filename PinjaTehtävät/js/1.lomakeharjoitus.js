b4cba1125d669c4f061c8382f67b4b9946e644b7
unknown <lari.felt@edu.keuda.fi>
2023-11-01 14:55:21 +0200

    lomake

---
+++ PinjaTehtävät/js/1.lomakeharjoitus.js
@@ -0,0 +1,27 @@
+﻿
+    function tarkistus() {
+    var etunimi = document.getElementById("etunimi").value;
+    var sukunimi = document.getElementById("sukunimi").value;
+    var kulkupeli = document.querySelector('input[name="kulkupeli"]:checked');
+    var lempitunti = document.getElementById("lempitunti");
+
+    if (etunimi === "") {
+        alert("Anna etunimesi");
+    return;
+    }
+
+    if (sukunimi === "") {
+        alert("Anna sukunimesi");
+    return;
+    }
+
+    if (!kulkupeli) {
+        alert("Valitse kulkuväline");
+    return;
+    }
+
+    if (lempitunti.value === "") {
+        alert("Valitse lempituntisi");
+    return;
+    }
+}
